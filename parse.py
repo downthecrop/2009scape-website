9bc9938ccc05f50e925986ac2048b4734c6238a2
downthecrop <bryce@mathx.ca>
2021-09-18 02:30:39 -0700

    Jekyll templates

--- parse.py
+++ parse.py
@@ -6,15 +6,12 @@ from unidecode import unidecode
 from os import listdir
 from os.path import isfile, join
 
-mypath = "C:\\Users\\Anon\\Projects\\2009scape-website\\services\\m=news\\archives"
+mypath = "C:\\Users\\Anon\\Projects\\2009scape-website\\site\\community\\"
 
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
- 
-url = 'http://localhost:8000/services/m=news/archives/2020-11-19.html'
-
 
 for f in onlyfiles:
-    url = "http://localhost:8000/services/m=news/archives/" + f
+    url = "http://localhost:8000/site/community/" + f
     print(url)
     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"}
     response = requests.get(url, headers=headers)
@@ -24,11 +21,11 @@ for f in onlyfiles:
     tmp = open("test/"+f, "w")
     tmp.write('---\n')
     tmp.write('title: '+soup.title.string.replace("2009scape - ","")+'\n')
-    tmp.write('tags: news'+'\n')
-    tmp.write('layout: newspost'+'\n')
-    tmp.write('collection: Game Updates'+'\n')
-    tmp.write('date: '+f[:-5]+" 00:00:00 +0000"+'\n')
-    tmp.write('authors: '+soup.find("div", {"class": "msgcreator uname"}).text.strip()+"\n")
+    tmp.write('tags: community'+'\n')
+    tmp.write('layout: guide'+'\n')
+    #tmp.write('collection: Game Updates'+'\n')
+    #tmp.write('date: '+f[:-5]+" 00:00:00 +0000"+'\n')
+    #tmp.write('authors: '+soup.find("div", {"class": "msgcreator uname"}).text.strip()+"\n")
     tmp.write('---\n')
     for hit in soup.findAll(attrs={'id' : 'content'}):
         tmp.write(unidecode(str(hit)))
